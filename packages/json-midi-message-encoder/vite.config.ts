53ce1f4aeeaeedbbcb18631ad501efd5e5e5c73c
Andrew Angelle <andrewangelle@gmail.com>
2024-06-22 15:10:57 -0400

    Migrate to Turborepo (#5)
    * Migrate to Turborepo
    
    * changeset for 1.0.4

--- packages/json-midi-message-encoder/vite.config.ts
+++ packages/json-midi-message-encoder/vite.config.ts
@@ -1,5 +1,4 @@
 import * as path from 'node:path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
@@ -9,20 +8,12 @@ export default defineConfig({
   cacheDir: '../../node_modules/.vite/packages/json-midi-message-encoder',
 
   plugins: [
-    nxViteTsPaths(),
     dts({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
   ],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
     outDir: './dist',
     emptyOutDir: true,
@@ -31,16 +22,12 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
     lib: {
-      // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
       name: 'json-midi-message-encoder',
       fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      // External packages that should not be bundled into your library.
       external: ['node:worker_threads', 'node:events'],
     },
   },
@@ -48,9 +35,6 @@ export default defineConfig({
   test: {
     watch: false,
     globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest/packages/json-midi-message-encoder',
-    },
     environment: 'node',
     include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
