be08e5e09a1c0fa3ab55492fcf64423b390941c5
Andrew Angelle <andrewangelle@gmail.com>
2026-05-05 09:40:15 -0400

    fix(test-utils): jsr publish failure

--- packages/test-utils/src/helper/load-fixture.ts
+++ packages/test-utils/src/helper/load-fixture.ts
@@ -2,13 +2,18 @@ import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import type { MidiFile } from '@midi-json-tools/types';
 
-export async function loadFixtureAsArrayBuffer(fixture: string) {
+export async function loadFixtureAsArrayBuffer(
+  fixture: string,
+): Promise<ArrayBuffer> {
   const filename = join(import.meta.dirname, 'fixtures', fixture);
   const response = await readFile(filename);
-  return response.buffer;
+  return response.buffer.slice(
+    response.byteOffset,
+    response.byteOffset + response.byteLength,
+  );
 }
 
-export async function loadFixtureAsJson(fixture: string) {
+export async function loadFixtureAsJson(fixture: string): Promise<MidiFile> {
   const filename = join(import.meta.dirname, 'fixtures', fixture);
   const response = await readFile(filename);
   return JSON.parse(response.toString()) as unknown as MidiFile;
